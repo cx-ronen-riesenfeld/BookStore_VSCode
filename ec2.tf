6961347136ae8bca3ec99e100c227d35e1f204eb
Ronen Riesenfeld <juanpablo@gmail.com>
2025-06-03 14:26:49 -0600

    agregue secreto

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_host" {
-  # ec2 have plain text secrets in user data1
+  # ec2 have plain text secrets in user data2
   ami           = "${var.ami}"
   instance_type = "t2.nano"
 
