c6b4f0d6392f643b4b4de2cf2c772707f197c357
Ronen Riesenfeld <juanpablo@gmail.com>
2025-06-04 14:13:36 -0600

    secrets

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_host" {
-  # ec2 have plain text secrets in user data3
+  # ec2 have plain text secrets in user data 123
   ami           = "${var.ami}"
   instance_type = "t2.nano"
 
